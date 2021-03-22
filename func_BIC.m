994851c7642f0ba0b78c9b4a32f6a11e6c310ea9
Weiting <54346415+weiting1029@users.noreply.github.com>
2021-03-22 12:37:46 +0100

    add bic function

---
+++ func_BIC.m
@@ -0,0 +1,10 @@
+function [p1,p2,BIC] = func_BIC(inputArg1,inputArg2)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+
+
+
+end
+
--- main.m
+++ main.m
@@ -3,9 +3,9 @@
 k = 10;
 N=10;% number of individuals
 T=100;% time horizon 
-rho0=0.8;% endogenous social effect
+rho0=0;% endogenous social effect
 beta0=0.8;% measures sensitivity to its own characteristic
-gamma0=0;% exogenous social effect
+gamma0=0.5;% exogenous social effect
 I = eye(N);
 unit = ones([N,1]);
 zero = zeros([N,1]);
@@ -44,9 +44,13 @@ Y = zeros([T,N]);
 Y = func_gnr_dgp(beta0,gamma0,rho0,X,rd_network1,Err,Y);
 
 %estimation
-p1 = 10;
+p1 = 30;
 p2 = 100;
-x_initial = x_true;  
+rng(1)
+test = func_gnr_rnd_network(N);
+x_initial = x_true;
+% x_initial(1:end-3) = func_remove_diag(test,N);
+x_initial(1:end-3) = zeros([N*(N-2),1]);
 x_initial(end-2:end)= 0;
 % x_initial = x_initial';
 
@@ -58,7 +62,7 @@ estimate_wlist = x_gmm(1:end-3);
 estimate_network = func_reconstruct(estimate_wlist,N);
 
 
-x_initial_slack = [x_true(1:end-3);x_true(1:end-3);x_initial(end-2:end)];
+x_initial_slack = [x_initial(1:end-3);x_initial(1:end-3);x_initial(end-2:end)];
 f_min_gmm_slack = @(x)func_gmm_lasso_stage_one_slack(x,Y,X,p1);
 [x_gmm_slack, obj_gmm_slack] = func_min_pen_obj_slack(f_min_gmm_slack,x_initial_slack,N);
 estimate_scalar_slack = x_gmm_slack(end-2:end);
